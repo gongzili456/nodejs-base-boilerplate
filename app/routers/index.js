c1ff6c7792cf1fdb73a318c7a437e7390fbdc632
liuguili <gongzili456@gmail.com>
2018-05-10 10:25:26 +0800

    feat(docker): Mapping local dir to work dir in docker for watch file to restart

--- app/routers/index.js
+++ app/routers/index.js
@@ -9,37 +9,37 @@ const debug = new Debug('app:app:routers:index:')
 
 const ROUTERS = new RequireDir()
 const CONTROLLERS = new RequireDir('../controllers', {
-    recurse: true,
+  recurse: true,
 })
 
 debug('ROUTERS: ', ROUTERS)
 
-export default () => { 
-    const router = new Router({
-        prefix: '/api/v1',
-    })
+export default () => {
+  const router = new Router({
+    prefix: '/api/v1',
+  })
 
-    /**
-     * Load other routers
-     */
-    Object.keys(ROUTERS).map(rkey => {
-        const _router = ROUTERS[rkey](validator.validate, CONTROLLERS)
-        return router.use('', _router.routes(), _router.allowedMethods())
-    })
+  /**
+   * Load other routers
+   */
+  Object.keys(ROUTERS).map(rkey => {
+    const _router = ROUTERS[rkey](validator.validate, CONTROLLERS)
+    return router.use('', _router.routes(), _router.allowedMethods())
+  })
 
-    /**
-     * Man Router
-     */
-    router.get('/hello', async (ctx, next) => {
-        ctx.body = {
-            statusCode: 200,
-            data: 'Hello World.',
-        }
-    })
+  /**
+   * Man Router
+   */
+  router.get('/hello', async (ctx, next) => {
+    ctx.body = {
+      statusCode: 200,
+      data: 'Hello World.',
+    }
+  })
 
-    router.get('/error', async (ctx, next) => {
-        throw new HttpError(400)
-    })
+  router.get('/error', async (ctx, next) => {
+    throw new HttpError(400)
+  })
 
-    return router
+  return router
 }
